491f1d1fcaaa4189c64ef93dd57d8813f548582c
unknown <dorian.goninet@ut-capitole.fr>
2018-02-04 18:27:30 +0100

    v5 updated: NA's removed

--- happiness.R
+++ happiness.R
@@ -1,4 +1,4 @@
-load.libraries <- c('gdata', 'ggplot2', 'xlsx', 'MNP')
+load.libraries <- c('gdata', 'ggplot2', 'xlsx', 'MNP', 'psych')
 install.lib <- load.libraries[!load.libraries %in% installed.packages()]
 for(libs in install.lib) install.packages(libs, dependencies = TRUE)
 sapply(load.libraries, require, character = TRUE)
@@ -12,7 +12,7 @@ finaldata <- merge(data, ginibase, by.x=c('Country', 'Year'), by.y=c('Country', 
 finaldata <- finaldata[, c(1:2, 4:5, 10)]
 # Remove duplicates
 duplicates <- which(duplicated(finaldata))
-finaldata<-finaldata[-duplicates,]
+finaldata <- finaldata[-duplicates,]
 
 co2base <- read.csv('database-co2.csv')
 finaldata <- merge(finaldata, co2base, by.x=c('Country', 'Year'), by.y=c('Entity', 'Year'))
@@ -20,6 +20,32 @@ finaldata <- rename.vars(finaldata, c('COâ...emissions.per.capita..per.year...t
 finaldata <- remove.vars(finaldata, 'Code')
 
 write.xlsx(finaldata, file = 'database-whr17-v2.xlsx', col.names = TRUE, row.names = FALSE)
+
+
+
+cpwbase <- read.csv('database-cpw.csv')
+
+finaldata <- merge(finaldata, cpwbase, by.x=c('Country', 'Year'), by.y=c('Entity', 'Year'))
+finaldata <- finaldata[, c(1:7, 9)]
+finaldata <- rename.vars(finaldata, c('Estimates..1950...2015..Demographic.Indicators...Total.fertility..live.births.per.woman...live.births.per.woman.'), c('CPW'))
+
+write.xlsx(finaldata, file = 'database-whr17-v4.xlsx', col.names = TRUE, row.names = FALSE)
+
+# Remove the NAs
+finaldata <- read.xlsx('database-whr17-v4.xlsx', 1)
+finaldata <- na.omit(finaldata)
+write.xlsx(finaldata, file = 'database-whr17-v5.xlsx', col.names = TRUE, row.names = FALSE)
+
+
+
+
+# Load final database
+finaldata <- read.xlsx('database-whr17-v5.xlsx', 1)
+
+# Statistics
+summary(finaldata)
+describe(finaldata)
+
 
 # Multi-nominal probit
 mnp <- mnp(RLadder ~ GDP + GINI + CO2, finaldata)
